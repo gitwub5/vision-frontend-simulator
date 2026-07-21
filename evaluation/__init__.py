8a3c0c48e1e1ce135606a3d339e42b89bf0fa564
eujin <pinklusha999@gmail.com>
2026-07-21 16:56:31 +0900

    Add comparison report generation

--- evaluation/__init__.py
+++ evaluation/__init__.py
@@ -1,1 +1,19 @@
 """Evaluation metric helpers."""
+
+from evaluation.comparison_report import (
+    ComparisonInputs,
+    ComparisonReport,
+    build_comparison_report,
+    read_detection_jsonl,
+    write_report_json,
+    write_report_markdown,
+)
+
+__all__ = [
+    "ComparisonInputs",
+    "ComparisonReport",
+    "build_comparison_report",
+    "read_detection_jsonl",
+    "write_report_json",
+    "write_report_markdown",
+]
